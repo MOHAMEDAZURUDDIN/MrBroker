62e00f35ef073176ee3a50220752b7209be523b8
MOHAMEDAZURUDDIN <ajstackdev7@gmail.com>
2025-06-29 11:52:37 +0530

    updated build file

--- frontend/src/components/layout/GetBackgroundColor.jsx
+++
@@ -1,8 +0,0 @@
-import { useSelector } from "react-redux";
-
-const GetBackgroundColor = () => {
-  const darkMode = useSelector((state) => state.darkmode);
-  return darkMode ? "bg-gray-300" : "bg-slate-950";
-};
-
-export default GetBackgroundColor;
